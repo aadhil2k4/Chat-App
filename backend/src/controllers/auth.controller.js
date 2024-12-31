398b9d3b2dff78cb2fff00f3624ccf727f2ad9ca
Aadhil Ahamed <aadhil2k4@gmail.com>
2024-12-31 23:49:12 +0530

    rectifications in signup route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -5,13 +5,16 @@ import bcrypt from 'bcryptjs'
 export const signup = async (req,res) => {
     const {fullName,email,password} = req.body 
     try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: 'All fields are required'});
+        }
         if(password.length<6){
             return res.status(400).json({message: 'Password must be atleast 6 characters'});
         }
         const user = await User.findOne({email});
         if(user) return res.status(400).json({message: 'Email already exists'});
-        const salt = bcrypt.genSalt(10);
-        const hashedPassword = bcrypt.hash(password, salt);
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
         const newUser = new User({
             fullName,
             email,
