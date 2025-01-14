ab4ec94c01e6b420fe1c3bb7c78e469ed666cbae
Aadhil Ahamed <aadhil2k4@gmail.com>
2025-01-14 19:50:02 +0530

    Profile Page UI

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -55,5 +55,9 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message);
         }
+    },
+
+    updateProfile: async (data) => {
+        
     }
 }))
