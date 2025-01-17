1e7d20f3d2f89d2d8f8404d3d21d5f2d0c109192
Aadhil Ahamed <aadhil2k4@gmail.com>
2025-01-18 00:54:27 +0530

    socket.io functionality implementation

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,5 +1,5 @@
 import { useChatStore } from "../store/useChatStore";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./Skeletons/MessageSkeleton";
@@ -7,13 +7,33 @@ import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-  const { messages, getMessages, isMessagesLoading, selectedUser } =
-    useChatStore();
+  const {
+    messages,
+    getMessages,
+    isMessagesLoading,
+    selectedUser,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  } = useChatStore();
   const { authUser } = useAuthStore();
+  const messageEndRef = useRef(null);
 
   useEffect(() => {
     getMessages(selectedUser._id);
-  }, [selectedUser._id, getMessages]);
+    subscribeToMessages();
+    return () => unsubscribeFromMessages();
+  }, [
+    selectedUser._id,
+    getMessages,
+    subscribeToMessages,
+    unsubscribeFromMessages,
+  ]);
+
+  useEffect(() => {
+    if(messageEndRef.current && messages){
+    messageEndRef.current.scrollIntoView({behavior: "smooth"});
+    }
+  },[messages]);
 
   if (isMessagesLoading) {
     return (
@@ -34,6 +54,7 @@ const ChatContainer = () => {
             className={`chat ${
               message.senderId === authUser._id ? "chat-end" : "chat-start"
             }`}
+            ref={messageEndRef}
           >
             <div className=" chat-image avatar">
               <div className="size-10 rounded-full border">
@@ -54,7 +75,11 @@ const ChatContainer = () => {
             </div>
             <div className="chat-bubble flex flex-col">
               {message.image && (
-                <img src={message.image} alt="Attachement" className="sm:max-w-[200px] rounded-md mb-2" />
+                <img
+                  src={message.image}
+                  alt="Attachement"
+                  className="sm:max-w-[200px] rounded-md mb-2"
+                />
               )}
               {message.text && <p>{message.text}</p>}
             </div>
